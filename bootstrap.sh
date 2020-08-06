e38bddaa00e2c97b7b5055136fdefc05cf4ef1e7
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2020-08-06 17:09:52 -0300

    A few command will be run in my ansible role

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,16 +17,4 @@ mkdir -p "$HOME/dev/src/github.com/giulianisanches"
     "$HOME/.local/bin/ansible-playbook" -K ./home_sweet_home.yml
 )
 
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
-# Set flameshot as the default screenshot tool
-# https://askubuntu.com/questions/1036473/ubuntu-18-how-to-change-screenshot-application-to-flameshot
-gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/flameshot gui'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
-# disable gnome keyring for ssh
-cp /etc/xdg/autostart/gnome-keyring-ssh.desktop .config/autostart/
-echo "Hidden=true" >> ~/.config/autostart/gnome-keyring-ssh.desktop
-
 chsh -s $( which zsh )
