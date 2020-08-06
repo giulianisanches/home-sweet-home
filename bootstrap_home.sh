c30a9cd1d6a0bfe1e8d1a8890582fc89cbd77084
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2020-08-05 23:44:12 -0300

    WIP

--- bootstrap_home.sh
+++ bootstrap_home.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-sudo apt install curl wget python3-pip git
+sudo apt install curl wget python3-pip git zsh
+
+mkdir "$HOME/.zinit"
+git clone https://github.com/zdharma/zinit.git  "$HOME/.zinit/bin"
 
 pip3 install --user awscli
 pip3 install --user ansible
 
-mkdir -p "$HOME/dev/src/github.com/giulianisanhes"
+mkdir -p "$HOME/dev/src/github.com/giulianisanches"
 
 (
     cd "$HOME/dev/src/github.com/giulianisanches"
-    git clone 
+    git clone git@gitlab.com:giulianideon/home-sweet-home.git
 )
+
+chsh -s $( which zsh )
