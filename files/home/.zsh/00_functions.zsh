618eff28e8247ace798193da6d29c49d40cd9495
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2020-08-06 12:01:53 -0300

    Moved my bash function to here (as-is)

--- files/home/.zsh/00_functions.zsh
+++ files/home/.zsh/00_functions.zsh
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
 function __function_exists() {
     declare -f -F $1 > /dev/null
     return $?
 }
 
-function __ldap_search () {
+function ldap_search () {
   local op="$1"
   local field="$2"
   local filter="$3"
@@ -23,20 +23,7 @@ function __ldap_search () {
   esac
 }
 
-function __vault() {
-  echo "..."
-  # local op="$1"; [[ $op eq "setenv"]] && shift
-  # local stage="${1,,}"; shift
-  # local parameters="$*"
-
-  # [[ -d $HOME/.config/vault ]] || mkdir -p "$HOME/.config/vault" && touch "$HOME/.config/vault/tokens"
-
-  # export VAULT_ADDR="https://vault.${stage}-sicredi.in:8200"
-  #   vault login -method=ldap username="${LDAP_USERNAME}"
-  #   vault token renew
-}
-
-function __pyenv() {
+function pyenv() {
   [[ -z ${VENV_HOME}  ]] && echo "Set VENV_HOME environment variable" && return 1
 
   local op="${1,,}"
@@ -66,4 +53,30 @@ function __pyenv() {
             pyenv (create|list|activate|remove) <nome_virtualevn>"
 		;;
   esac
+}
+
+function c8s() {
+    if [[ -z $1 ]]; then
+        echo "Ambiente não especificado."
+    else
+        KUBECONFIG_DIR="$GITLAB/devops_st/st-cas-k8s-kubespray/inventory"
+        if [[ "$1" == "dev" ]]; then
+            export KUBECONFIG_FILE="$KUBECONFIG_DIR/cluster_dev/artifacts/admin.conf"
+        elif [[ "$1" == "tst" ]]; then
+            export KUBECONFIG_FILE="$KUBECONFIG_DIR/cluster_tst/artifacts/admin.conf"
+        elif [[ "$1" == "uat" ]]; then
+            export KUBECONFIG_FILE="$KUBECONFIG_DIR/cluster_uat/artifacts/admin.conf"
+        elif [[ "$1" == "prd" ]]; then
+            export KUBECONFIG_FILE="$KUBECONFIG_DIR/cluster_prd/artifacts/admin.conf"
+        else
+            echo "Ambiente inválido."
+        fi
+        kubectl --kubeconfig $KUBECONFIG_FILE ${@:2}
+    fi
+}
+
+function find_elb_endpoint() {
+    local elb_arn
+    
+    mapfile -t elb_arn < <(aws elbv2 describe-load-balancers --query 'LoadBalancers[?starts_with(LoadBalancerName, `DEV-ECS-CLUSTER-SVC`)].{Arn:LoadBalancerArn}' --output text)
 }
