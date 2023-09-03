520980a057f1df6605b11725f138da259f75199a
Giuliani D. Sanches <giulianisanches@userss.noreply.github.com>
2023-09-03 13:59:31 -0300

    feat: install ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-distribution="$(lsb_release -is 2>& /dev/null | tr '[:upper:]' '[:lower:]')"
+distribution="$(lsb_release -is 2> /dev/null | tr '[:upper:]' '[:lower:]')"
+
+PATH="$PATH:$HOME/.local/bin"
+export PATH
 
 if [[ "$distribution" == 'fedora' ]]
 then
@@ -11,8 +14,11 @@ fi
 if [[ "$distribution" == 'debian' ]]
 then
     sudo apt-get -y upgrade
-    sudo apt-get -y install sudo python3-pip git zsh curl gconf2
+    sudo apt-get -y install sudo python3-pip pipx git zsh curl gconf2
 fi
+
+# install ansible
+pipx install --include-deps --force ansible
 
 zini_home="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [[ ! -d "$zini_home" ]] && mkdir -p "$(dirname "$zini_home")"
@@ -20,13 +26,13 @@ zini_home="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
     && git clone https://github.com/zdharma-continuum/zinit.git "$zini_home"
 
 
-home_sweet_home="$HOME/dev/src/github.com/giulianisanches"
+home_sweet_home="$HOME/dev/src/github.com/giulianisanches/home-sweet-home"
 [[ ! -d "$home_sweet_home" ]] \
-    && mkdir -p "$HOME/dev/src/github.com/giulianisanches"
+    && mkdir -p "$home_sweet_home"
 
 [[ ! -d "$home_sweet_home/.git" ]] \
-    && git clone -C "$home_sweet_home" \
-        https://github.com/giulianisanches/home-sweet-home.git
+    && git clone \
+        https://github.com/giulianisanches/home-sweet-home.git "$home_sweet_home"
 
 (
     cd "$HOME/dev/src/github.com/giulianisanches" || exit
@@ -34,7 +40,7 @@ home_sweet_home="$HOME/dev/src/github.com/giulianisanches"
     # change the clone URL to use https
     git clone git@github.com:giulianisanches/home-sweet-home.git
     cd home-sweet-home || exit
-    #ansible-playbook -K './home_sweet_home.yml'
+    ansible-playbook -K './home_sweet_home.yml'
 )
 
 sudo chsh -s "$( which zsh )"
