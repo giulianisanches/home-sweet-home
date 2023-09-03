a69d6e701e2770bc9486101663b418ec63cb434c
Giuliani D. Sanches <giulianisanches@userss.noreply.github.com>
2023-09-03 12:49:12 -0300

    feat: improve bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo dnf install python3-pip git zsh util-linux-user
+sudo apt-get -y upgrade
+sudo apt-get -y install sudo python3-pip git zsh curl gconf2
+
+[[ -d "$HOME/.zinit" ]] && rm -rf "$HOME/.zinit"
 
 mkdir "$HOME/.zinit"
 git clone https://github.com/zdharma-continuum/zinit.git  "$HOME/.zinit/bin"
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -r ./aws
-rm -f awscliv2.zip
-
-pip3 install --user ansible
+[[ -d "$HOME/dev/src/github.com/giulianisanches" ]] \
+    && rm -rf "$HOME/dev/src/github.com/giulianisanches"
 
 mkdir -p "$HOME/dev/src/github.com/giulianisanches"
-
 (
-    cd "$HOME/dev/src/github.com/giulianisanches"
-    # if you're planning to clone my repository, change the clone URL to use https
+    cd "$HOME/dev/src/github.com/giulianisanches" || exit
+    # if you're planning to clone my repository
+    # change the clone URL to use https
     git clone git@github.com:giulianisanches/home-sweet-home.git
-    cd home-sweet-home
-    ansible-playbook -K './home_sweet_home.yml'
+    cd home-sweet-home || exit
+    #ansible-playbook -K './home_sweet_home.yml'
 )
 
-mkdir -p "$HOME/dev/src/github.com/Gogh-Co"
-(
-    cd "$HOME/dev/src/github.com/Gogh-Co"
-    git clone https://github.com/Gogh-Co/Gogh.git
-    cd Gogh/themes
-
-    export TERMINAL=gnome-terminal
-    ./dracula.sh
-)
-
-chsh -s $( which zsh )
+sudo chsh -s "$( which zsh )"
