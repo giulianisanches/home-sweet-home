7a2e46eee199f5fa48a128a55d3bbe678c7f4a8a
Giuliani D. Sanches <giulianisanches@userss.noreply.github.com>
2024-04-02 07:33:21 -0300

    feat: update to gnome configurations

---
+++ roles/desktop/files/scripts/set_default_gnome_terminal_profile.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+read -r -a profile_list <<< "$(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "',[]")"
+for profile_id in "${profile_list[@]}"
+do
+    profile_name=$(gsettings get org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"$profile_id"/ visible-name | tr -d "'")
+    if [[ "$profile_name" == 'Dracula' ]]
+    then
+        gsettings set org.gnome.Terminal.ProfilesList default "$profile_id"
+        break
+    fi
+done
