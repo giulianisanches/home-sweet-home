6e1a697d544473f18dc3897d2ad9c207087631d7
Giuliani D. Sanches <giulianisanches@userss.noreply.github.com>
2026-09-26 12:17:34 -0300

    feat: install pyton3-debian for new debian tasks

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 PATH="$PATH:$HOME/.local/bin"
 export PATH
 
-sudo apt install -y sudo python3-pip pipx git zsh curl
+sudo apt install -y sudo python3-debian python3-pip pipx git zsh curl
 
 # install ansible
 pipx install --include-deps --force ansible
